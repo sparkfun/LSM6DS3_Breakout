998e8676c0dd9f9269a9f35a1f6b4d51c51e9646
marshalltaylorSFE <marshall.taylor@sparkfun.com>
2015-04-16 14:30:06 -0600

    Hollow I2C functions

--- Test Sketches/proto1_bringup1/proto1_bringup1.ino
+++ Test Sketches/proto1_bringup1/proto1_bringup1.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+// Wire Master Reader
+// by Nicholas Zambetti <http://www.zambetti.com>
 
+// Demonstrates use of the Wire library
+// Reads data from an I2C/TWI slave device
+// Refer to the "Wire Slave Sender" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+  Serial.write("Test Phrase");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Wire.requestFrom(0x6B, 6);    // request 6 bytes from slave device #2
 
+  while (Wire.available())   // slave may send less than requested
+  {
+    char c = Wire.read(); // receive a byte as character
+    Serial.print(c, HEX);         // print the character
+  }
+
+  delay(500);
 }
